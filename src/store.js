d48a4315570e4b42563222a8f0e2dfdd2ee35816
Tobi Wan <tobi.klatt@web.de>
2019-05-17 18:03:47 +0200

    added basic crud operations

--- src/store.js
+++ src/store.js
@@ -1,14 +1,66 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from '@/firebase.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    corgis: null
+  },
 
-  mutations: {},
+  mutations: {
+    setCorgis(state, payload) { state.corgis = payload }
+  },
 
-  actions: {},
+  actions: {
+    // refactor
 
-  getters: {}
+    login (context, payload) {
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then(payload => { context.commit('setUser', { id: payload.user.uid }) })
+        .catch(error => { console.log(error) })
+    },
+
+    async getCorgies(context) {
+      return new Promise((resolve, reject) => {
+        firebase.firestore().collection('corgis').get()
+          .then(snapshot => {
+            const corgis = []
+            for (let document of snapshot.docs) corgis.push({ id: document.id, name: document.data().name })
+            context.commit('setCorgis', corgis)
+            resolve()
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      })
+    },
+
+    async uploadCorgi(context, payload) {
+      firebase.firestore().collection('corgis').add(payload)
+        .then(payload => { resolve(payload.id) })
+        .catch(error => { console.log(error) })
+    },
+
+    async updateCorgi (context, payload) {
+      return new Promise((resolve, reject) => {
+        firebase.firestore().collection('corgis').doc(payload.corgiId).update(payload.newData)
+          .then(payload => { resolve(payload.id) })
+          .catch(error => { console.log(error) })
+      })
+    },
+
+    async deleteCorgi (context, payload) {
+      return new Promise((resolve, reject) => {
+        firebase.firestore().collection('corgis').doc(payload.corgiId).delete(payload.newData)
+          .then(payload => { resolve(payload.id) })
+          .catch(error => { console.log(error) })
+      })
+    },
+  },
+
+  getters: {
+    corgis (state) { return state.corgis }
+  }
 })
