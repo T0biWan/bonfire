c5d20079ad6e2badc0385ac4084d8a6fc5ae8bca
Tobias Klatt <tobi.klatt@web.de>
2019-11-23 13:41:16 +0100

    added new approach to vuex api communication

--- src/components/Showcase.vue
+++ src/components/Showcase.vue
@@ -6,19 +6,12 @@
         <th>id</th><th>name</th>
       </thead>
       <tbody>
-        <tr v-for="soul in new_soul" :key="soul.id">
+        <tr v-for="soul in souls" :key="soul.id">
           <td>{{ soul.id }}</td>
           <td>{{ soul.name }}</td>
         </tr>
       </tbody>
     </table>
-
-    {{ $store.state.new_souls }}
-    <br>
-    {{ $store.state.active }}
-
-    <button @click="$store.commit('update_new_soul', {'new_soul_id': '3LdbeBGwaop3vFrxtNgB', 'new_soul': {'id': '3LdbeBGwaop3vFrxtNgB', 'name': 'Tobi' }})">Click Mich</button>
-
   </div>
 </template>
 
@@ -27,22 +20,20 @@ export default {
   name: 'Showcase',
 
   created () {
-    // this.$store.dispatch('create_soul', { name: 'Korvo' })
-    this.$store.dispatch('get_souls')
-    // this.$store.dispatch('get_soul_by_id', 'D56zJTGMluTUh8LRukqP')
-    // this.$store.dispatch('get_souls_by_field', { field: 'name', value: 'Kaddi'})
-    // this.$store.dispatch('update_soul', { id: 'lnsNTaw0z1li7n5WiggR', data: { name: '1' } })
-    // this.$store.dispatch('delete_soul', 'QTDZWFY8PLmYIo7GuKCn')
+    // this.$store.dispatch('souls/create_soul', { name: 'Korvo' })
+    this.$store.dispatch('souls/get_souls')
+    // this.$store.dispatch('souls/get_soul_by_id', 'D56zJTGMluTUh8LRukqP')
+    // this.$store.dispatch('souls/get_souls_by_field', { field: 'name', value: 'Kaddi'})
+    // this.$store.dispatch('souls/update_soul', { id: 'lnsNTaw0z1li7n5WiggR', data: { name: '1' } })
+    // this.$store.dispatch('souls/delete_soul', 'QTDZWFY8PLmYIo7GuKCn')
   },
 
   computed: {
-    souls () { return this.$store.getters.souls },
-    new_soul () { return this.$store.getters.new_souls }
+    souls () { return this.$store.getters['souls/souls'] }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 table {
   width: 30%;
--- src/store.js
+++ src/store.js
@@ -1,78 +1,174 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as api from '@/api/souls-api'
+import * as firebase from '@/api/firebase'
+import * as soulModel from '@/models/soul'
 
 Vue.use(Vuex)
-https://medium.com/js-dojo/structuring-vuex-modules-for-relationships-speed-and-durability-de25f7403643
-https://vuejs.org/v2/guide/reactivity.html
-https://vuejs.org/v2/guide/list.html#Object-Change-Detection-Caveats
-export default new Vuex.Store({
-  state: {
-    souls: null,
-    count: 0,
 
-    all: {},
-    active: false,
+const souls_module = {
+  module: {
+    namespaced: true,
 
-    new_souls: {},
-    active_new_soul: false
+    state: {
+      souls: {},
+      active_soul_id: false
+    },
+
+    getters: {
+      souls (state) { return state.souls },
+      active_soul (state) { return state.active_soul_id ? state.souls[state.active_soul_id] : false }
+    },
+
+    mutations: {
+      add_soul (state, soul) { Vue.set(state.souls, soul.id, soul) },
+      delete_soul (state, soul) { Vue.delete(state.souls, soul.id) },
+      set_active_soul (state, soul) { state.active_soul_id = soul.id },
+      update_soul (state, soul) { state.souls[soul.id] = soul }
+    },
+
+    actions: {
+      async get_souls (context) {
+        const souls = await souls_module.api.get_souls()
+        for (let soul of souls) context.commit('add_soul', soul)
+      },
+
+      async new_soul (context) {
+        let soul = { author: 'tobi', created: moment().valueOf(), edited: moment().valueOf(), markdown: '' }
+        const id = await souls_module.api.create_soul(soul)
+        soul = await souls_module.api.get_soul_by_id(id)
+        context.commit('add_soul', soul)
+        context.commit('set_active_soul', soul)
+      },
+
+      async save_soul (context) {
+        let soul = context.getters.active_soul
+        await souls_module.api.update_soul(soul.id, { markdown: soul.markdown, edited: moment().valueOf() })
+        soul = await souls_module.api.get_soul_by_id(soul.id)
+        context.commit('update_soul', soul)
+      },
+
+      async delete_soul (context) {
+        let soul = context.getters.active_soul
+        await souls_module.api.delete_soul(soul.id)
+        context.commit('delete_soul', soul)
+        context.commit('set_active_soul', false)
+      }
+    }
   },
 
-  getters: {
-    souls (state) { return state.souls },
+  api: {
+    get_souls: async function () {
+      const documents = await firebase.read('souls')
+      const souls = documents.map(document => souls_module.parser.instantiateNote(document))
+      return souls
+    },
 
-    new_souls (state) { return state.new_souls },
-    new_soulById (state, id) { return state.all[id] || null },
-    active_new_soul (state, id) { return state.active ? state.all[state.active] : false }
-  },
-
-  mutations: {
-    increment (state) { state.count++ },
-    set_souls (state, payload) { state.souls = payload },
-
-    set_active_new_soul (state, new_soul_id) { state.active_new_soul = new_soul_id },
-    flush_new_souls (state) { state.new_souls = {} },
-    add_new_soul (state, {new_soul_id, new_soul}) { Vue.set(state.new_souls, new_soul_id, new_soul) },
-    update_new_soul (state, {new_soul_id, new_soul}) { state.new_souls[new_soul_id] = new_soul },
-    // remove_new_soul (state, new_soul_id) { delete state.new_souls[new_soul_id] }
-  },
-
-  actions: {
-    async create_soul (context, soul) {
-      const id = await api.create_soul(soul)
+    create_soul: async function (soul) {
+      const id = await firebase.create('souls', soul)
       return id
     },
 
-    all({commit}, all){
-      for(let album of all)
-        commit('add', album)
+    get_soul_by_id: async function (id) {
+      const document = await firebase.readById('souls', id)
+      const souls = parser.instantiateNote(document)
+      return souls
     },
 
-    async get_souls (context) {
-      const souls = await api.get_souls()
-      context.commit('set_souls', souls)
-
-      context.commit ('add_new_soul', {'new_soul_id': souls[0].id, 'new_soul': souls[0]})
-      context.commit ('add_new_soul', {'new_soul_id': souls[1].id, 'new_soul': souls[1]})
-      context.commit ('add_new_soul', {'new_soul_id': souls[2].id, 'new_soul': souls[2]})
+    get_souls_by_field: async function (field, value) {
+      const documents = await firebase.readByField('souls', field, value)
+      const souls = documents.map(document => parser.instantiateNote(document))
+      return souls
     },
 
-    async get_soul_by_id (context, id) {
-      const soul = await api.get_soul_by_id(id)
-      context.commit('set_souls', [soul])
+    update_soul: async function (id, data) {
+      await firebase.update('souls', id, data)
     },
 
-    async get_souls_by_field (context, payload) {
-      const souls = await api.get_souls_by_field(payload.field, payload.value)
-      context.commit('set_souls', souls)
-    },
+    delete_soul: async function (id) {
+      await firebase.remove('souls', id)
+    }
+  },
 
-    async update_soul (context, payload) {
-      await api.update_soul(payload.id, payload.data)
-    },
+  parser: {
+    instantiateNote: function (document) {
+      const id = document.id
+      const name = document.data().name
 
-    async delete_soul (context, id) {
-      await api.delete_soul(id)
+      return new soulModel.Soul(id, name)
     }
   }
+}
+
+export default new Vuex.Store({
+  modules: {
+    souls: souls_module.module
+  }
+  // state: {
+  //   souls: null,
+  //   count: 0,
+  //
+  //   all: {},
+  //   active: false,
+  //
+  //   new_souls: {},
+  //   active_new_soul: false
+  // },
+  //
+  // getters: {
+  //   souls (state) { return state.souls },
+  //
+  //   new_souls (state) { return state.new_souls },
+  //   new_soulById (state, id) { return state.all[id] || null },
+  //   active_new_soul (state, id) { return state.active ? state.all[state.active] : false }
+  // },
+  //
+  // mutations: {
+  //   increment (state) { state.count++ },
+  //   set_souls (state, payload) { state.souls = payload },
+  //
+  //   set_active_new_soul (state, new_soul_id) { state.active_new_soul = new_soul_id },
+  //   flush_new_souls (state) { state.new_souls = {} },
+  //   add_new_soul (state, {new_soul_id, new_soul}) { Vue.set(state.new_souls, new_soul_id, new_soul) },
+  //   update_new_soul (state, {new_soul_id, new_soul}) { state.new_souls[new_soul_id] = new_soul },
+  //   // remove_new_soul (state, new_soul_id) { delete state.new_souls[new_soul_id] }
+  // },
+  //
+  // actions: {
+  //   async create_soul (context, soul) {
+  //     const id = await api.create_soul(soul)
+  //     return id
+  //   },
+  //
+  //   all({commit}, all){
+  //     for(let album of all)
+  //       commit('add', album)
+  //   },
+  //
+  //   async get_souls (context) {
+  //     const souls = await api.get_souls()
+  //     context.commit('set_souls', souls)
+  //
+  //     context.commit ('add_new_soul', {'new_soul_id': souls[0].id, 'new_soul': souls[0]})
+  //     context.commit ('add_new_soul', {'new_soul_id': souls[1].id, 'new_soul': souls[1]})
+  //     context.commit ('add_new_soul', {'new_soul_id': souls[2].id, 'new_soul': souls[2]})
+  //   },
+  //
+  //   async get_soul_by_id (context, id) {
+  //     const soul = await api.get_soul_by_id(id)
+  //     context.commit('set_souls', [soul])
+  //   },
+  //
+  //   async get_souls_by_field (context, payload) {
+  //     const souls = await api.get_souls_by_field(payload.field, payload.value)
+  //     context.commit('set_souls', souls)
+  //   },
+  //
+  //   async update_soul (context, payload) {
+  //     await api.update_soul(payload.id, payload.data)
+  //   },
+  //
+  //   async delete_soul (context, id) {
+  //     await api.delete_soul(id)
+  //   }
+  // }
 })
